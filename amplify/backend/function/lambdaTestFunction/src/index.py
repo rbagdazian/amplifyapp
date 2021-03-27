d21ed3f8bd284276cddd0676eb6d30a2c7dd68ef
Richard Bagdazian <rbagdazian@gmail.com>
2021-03-27 02:03:05 +0000

    test 6

--- amplify/backend/function/lambdaTestFunction/src/index.py
+++ amplify/backend/function/lambdaTestFunction/src/index.py
@@ -18,7 +18,7 @@ def handler(event, context):
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
       },
-      'body': json.dumps({'timev': "ok to here"})
+      'body': json.dumps({'timev': 'ok to here', 'msg2': 'message2'})
   }
   
   
